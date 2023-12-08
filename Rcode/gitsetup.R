cf4543df41b95eae538664ee011657d37c9eeb0c
xqz9154 <aimanqiankun55@live.com>
2023-12-08 15:51:46 +0800

    shiny module to upload and download shapefile and preprocess DEM

--- Rcode/gitsetup.R
+++ Rcode/gitsetup.R
@@ -1,3 +1,11 @@
-install.packages("usethis")
+# install.packages("usethis")
 library(usethis)
+library(gitcreds)
 use_git_config(user.name = "xqz9154", user.email ="aimanqiankun55@live.com")
+use_git()
+create_github_token()
+
+# save 
+gitcreds::gitcreds_set()
+
+use_github()
